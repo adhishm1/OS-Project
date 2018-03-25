e7fdd25a3c385c93b2c2395b61382267a8b61eaf
adhishm1 <37334231+adhishm1@users.noreply.github.com>
2018-03-25 17:08:04 +0530

    Trying to create reader thread function

--- readerwriter.c
+++ readerwriter.c
@@ -12,3 +12,32 @@ void intialize()
     pthread_mutex_init(&ad,NULL);
     readcount=0;
 }
+
+void * reader(int * arg )
+{
+  pthread_mutex_lock(&x);
+    
+     if ( nw_active + nw_waiting == 0 )
+      {
+         nr_active++; // Notify we are active
+        
+      }
+    else
+      nr_waiting++;  // We are waiting
+
+pthread_mutex_unlock(&x); 
+
+
+   nr_active--;
+   if (( nr_active == 0 ) && ( nw_waiting > 0 )) // If we are the last reader
+     {
+        while ( nw_waiting > 0 )  // Allow all waiting writers to enter
+        {
+           
+           w_active++;  // one more active writer
+           w_waiting--; // one less waiting writer.
+        }
+      }
+   
+}
+
