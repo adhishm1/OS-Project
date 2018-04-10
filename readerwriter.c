1252c60efdb3a1c0c0e02c51c1f5bf9af0622cc1
adhishm1 <37334231+adhishm1@users.noreply.github.com>
2018-04-10 19:49:30 +0530

    Final Commit

--- readerwriter.c
+++ readerwriter.c
@@ -1,43 +1,117 @@
 #include<pthread.h>
+
+#include<unistd.h>
+
 #include<stdio.h>
+
 #include<stdlib.h>
 
-pthread_mutex_t x,ad;
-pthread_t tid;
+pthread_mutex_tx,wsem;
+
+pthread_ttid;
+
 int readcount;
 
 void intialize()
+
 {
-    pthread_mutex_init(&x,NULL);
-    pthread_mutex_init(&ad,NULL);
-    readcount=0;
+
+pthread_mutex_init(&x,NULL);
+
+pthread_mutex_init(&wsem,NULL);
+
+readcount=0;
+
 }
 
-void * reader(int * arg )
+void * reader (void * param)
+
 {
-  pthread_mutex_lock(&x);
-    
-     if ( nw_active + nw_waiting == 0 )
-      {
-         nr_active++; // Notify we are active
-        
-      }
-    else
-      nr_waiting++;  // We are waiting
 
-pthread_mutex_unlock(&x); 
+int waittime;
 
+waittime = rand() % 5;
 
-   nr_active--;
-   if (( nr_active == 0 ) && ( nw_waiting > 0 )) // If we are the last reader
-     {
-        while ( nw_waiting > 0 )  // Allow all waiting writers to enter
-        {
-           
-           w_active++;  // one more active writer
-           w_waiting--; // one less waiting writer.
-        }
-      }
-   
+printf("\nReader is trying to enter");
+
+pthread_mutex_lock(&x);
+
+readcount++;
+
+if(readcount==1)
+
+pthread_mutex_lock(&wsem);
+
+printf("\n%d Reader is inside ",readcount);
+
+pthread_mutex_unlock(&x);
+
+sleep(waittime);
+
+pthread_mutex_lock(&x);
+
+readcount--;
+
+if(readcount==0)
+
+pthread_mutex_unlock(&wsem);
+
+pthread_mutex_unlock(&x);
+
+printf("\nReader is Leaving");
+
 }
 
+void * writer (void * param)
+
+{
+
+int waittime;
+
+waittime=rand() % 3;
+
+printf("\nWriter is trying to enter");
+
+pthread_mutex_lock(&wsem);
+
+printf("\nWrite has entered");
+
+sleep(waittime);
+
+pthread_mutex_unlock(&wsem);
+
+printf("\nWriter is leaving");
+
+sleep(30);
+
+exit(0);
+
+}
+
+int main()
+
+{
+
+int n1,n2,i;
+
+printf("\nEnter the no of readers: ");
+
+scanf("%d",&n1);
+
+printf("\nEnter the no of writers: ");
+
+scanf("%d",&n2);
+
+for(i=0;i<n1;i++)
+
+pthread_create(&tid,NULL,reader,NULL);
+
+for(i=0;i<n2;i++)
+
+pthread_create(&tid,NULL,writer,NULL);
+
+sleep(30);
+
+exit(0);
+
+}
